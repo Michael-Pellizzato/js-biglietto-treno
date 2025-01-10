3f957a4eda339b28d21ca54b4a14574792acd28d
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-10 11:53:13 +0100

    calcoli inseriti

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//variabili
+// variabili
 const prezzobigliettokm = 0.21
 let km = parsent( prompt ( "quanti km devi percorrere?") )
 let età = parsent( prompt ( "quanti anni hai?") )
@@ -6,3 +6,13 @@ let scontominorenni = 0.80
 let scontoover65 = 0.60
 let prezzobiglietto;
 
+// calcoli
+
+if( età < 18 ) {
+    prezzobiglietto = (prezzobiglietto * km) * scontominorenni
+    console.log ( `il prezzo del tuo biglietto è: ${prezzobiglietto}`)
+} else if(età >65) {
+    prezzobiglietto = (prezzobiglietto * km) * scontoover65
+}else{
+    prezzobiglietto = (prezzobiglietto * km)
+}
