9b074c9d98d3f19bded7f4b053c3e639a6228abc
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-10 11:44:38 +0100

    preparazione esercizio

---
+++ script.js
@@ -0,0 +1,8 @@
+//variabili
+const prezzobigliettokm = 0.21
+let km = parsent( prompt ( "quanti km devi percorrere?") )
+let età = parsent( prompt ( "quanti anni hai?") )
+let scontominorenni = 0.80
+let scontoover65 = 0.60
+let prezzobiglietto;
+
