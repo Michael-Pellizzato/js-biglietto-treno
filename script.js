dcd2b7ad02775c26be27c6d5a38182f9cde1700b
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-10 12:42:03 +0100

    correzione esercizio con l'aiuto di Ale

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // variabili
 const prezzobigliettokm = 0.21
-let km = parseInt( prompt ( "quanti km devi percorrere") )
-let età = parseInt( prompt ( "quanti anni hai") )
+let km = parseInt ( prompt ( "quanti km devi percorrere") )
+let età = parseInt ( prompt ( "quanti anni hai") )
 let scontominorenni = 0.80
 let scontoover65 = 0.60
 let prezzobiglietto;
 
 // calcoli
 
-if( età < 18 ) {
-    prezzobiglietto = (prezzobiglietto * km) * scontominorenni
+if ( età < 18 ) {
+    prezzobiglietto = ( prezzobigliettokm * km ) * scontominorenni
     console.log ( `il prezzo del tuo biglietto è: ${prezzobiglietto}`)
-} else if(età >65) {
-    prezzobiglietto = (prezzobiglietto * km) * scontoover65
-}else{
-    prezzobiglietto = (prezzobiglietto * km)
+} else if ( età > 65 ) {
+    prezzobiglietto = ( prezzobigliettokm * km ) * scontoover65
+} else {
+    prezzobiglietto = ( prezzobigliettokm * km )
 }
 
 // output
-console.log(`il prezzo del tuo biglietto è ${prezzobiglietto.toFixed(2)}`)
+console.log (`il prezzo del tuo biglietto è ${prezzobiglietto.toFixed (2) }` )
