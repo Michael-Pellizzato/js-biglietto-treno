595e880809415eb4ccbc9724d1d7ce307f681f02
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-01-10 12:04:51 +0100

    correzione errori

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // variabili
 const prezzobigliettokm = 0.21
-let km = parsent( prompt ( "quanti km devi percorrere?") )
-let età = parsent( prompt ( "quanti anni hai?") )
+let km = parseInt( prompt ( "quanti km devi percorrere") )
+let età = parseInt( prompt ( "quanti anni hai") )
 let scontominorenni = 0.80
 let scontoover65 = 0.60
 let prezzobiglietto;
